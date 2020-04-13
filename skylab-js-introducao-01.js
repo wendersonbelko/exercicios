f125681e82f9870df80658010f608ecfc88feef2
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-04-12 23:00:19 -0300

    introdução ao javascript, skylab rocketseat.
    Crie uma função que dado o objeto a seguir:
    var endereco = {
     rua: "Rua dos pinheiros",
     numero: 1293,
     bairro: "Centro",
     cidade: "São Paulo",
     uf: "SP"
    };
    Retorne o seguinte conteúdo:
    O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com
    nº 1293.

---
+++ skylab-js-introducao-01.js
@@ -0,0 +1,15 @@
+var endereco = {
+ rua: "Rua dos pinheiros",
+ numero: 1293,
+ bairro: "Centro",
+ cidade: "São Paulo",
+ uf: "SP"
+};
+
+let rua = endereco.rua;
+let numero = endereco.numero;
+let bairro = endereco.bairro;
+let cidade = endereco.cidade;
+let uf = endereco.uf;
+
+console.log(`O usuario mora em ${cidade} / ${uf}, no bairro ${bairro}, na  "${rua}" com o numero ${numero}.`);
