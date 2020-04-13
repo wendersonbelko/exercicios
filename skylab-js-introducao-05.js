12f54d3008115a1dbd6a63a29e79f88aa9b092ba
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-04-12 23:04:17 -0300

    introdução ao javascript, skylab rocketseat.
    Dado o seguinte vetor de objetos:
    var usuarios = [
     {
     nome: "Diego",
     habilidades: ["Javascript", "ReactJS", "Redux"]
     },
     {
     nome: "Gabriel",
     habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
     }
    ];
    Escreva uma função que produza o seguinte resultado:
    O Diego possui as habilidades: Javascript, ReactJS, Redux
    O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir

---
+++ skylab-js-introducao-05.js
@@ -0,0 +1,12 @@
+var usuarios = [
+ {
+ nome: "Diego",
+ habilidades: ["Javascript", "ReactJS", "Redux"]
+ },
+ {
+ nome: "Gabriel",
+ habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+ }
+];
+console.log(`O ${usuarios[0].nome} possui as habilidades: ${usuarios[0].habilidades}`);
+console.log(`O ${usuarios[1].nome} possui as habilidades: ${usuarios[1].habilidades}`);
